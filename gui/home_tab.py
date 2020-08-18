73a7401cba9038f3b564c4c77d184cdfe7cb18fb
Mrestof <fotserm@gmail.com>
2020-08-18 18:10:45 +0300

    Fix position of labels.

--- gui/home_tab.py
+++ gui/home_tab.py
@@ -28,14 +28,14 @@ class HomeTab(wx.Panel):
         self.bot_selector_choice = wx.Choice(self, choices=self.bot_selectors)
         self.bot_selector_choice .SetSelection(0)
 
-        base_settings_sizer.Add(chat_mode_title, flag=wx.RIGHT | wx.TOP, border=10)
-        base_settings_sizer.Add(self.chat_mode_choice, 1, wx.TOP, 2)
-        base_settings_sizer.Add(bot_selector_title, flag=wx.LEFT | wx.RIGHT | wx.TOP, border=10)
-        base_settings_sizer.Add(self.bot_selector_choice, 1, wx.TOP, 2)
+        base_settings_sizer.Add(chat_mode_title, flag=wx.RIGHT | wx.ALIGN_CENTER, border=10)
+        base_settings_sizer.Add(self.chat_mode_choice, proportion=1)
+        base_settings_sizer.Add(bot_selector_title, flag=wx.LEFT | wx.RIGHT | wx.ALIGN_CENTER, border=10)
+        base_settings_sizer.Add(self.bot_selector_choice, proportion=1)
 
         main_sizer.Add(base_settings_sizer, flag=wx.EXPAND | wx.LEFT | wx.RIGHT | wx.BOTTOM, border=10)
 
-        # add panel to set detail configurations and to control the state of bots
+        # add big panel to set detail configurations and to control the state of bots
         more_control_sizer = wx.BoxSizer()
         settings_sizer = wx.BoxSizer(wx.VERTICAL)
         control_sizer = wx.BoxSizer(wx.VERTICAL)
@@ -52,11 +52,11 @@ class HomeTab(wx.Panel):
         self.td_iter_spin = wx.SpinCtrl(self, value='5000', max=3600*1000)
         self.repeats_spin = wx.SpinCtrl(self, max=9999999)
 
-        td_bot_sizer.Add(td_bot_title, flag=wx.LEFT | wx.RIGHT, border=10)
+        td_bot_sizer.Add(td_bot_title, flag=wx.LEFT | wx.RIGHT | wx.ALIGN_CENTER, border=10)
         td_bot_sizer.Add(self.td_bot_spin, 1, wx.RIGHT, 10)
-        td_iter_sizer.Add(td_iter_title, flag=wx.LEFT | wx.RIGHT, border=10)
+        td_iter_sizer.Add(td_iter_title, flag=wx.LEFT | wx.RIGHT | wx.ALIGN_CENTER, border=10)
         td_iter_sizer.Add(self.td_iter_spin, 1, wx.RIGHT, 10)
-        repeats_sizer.Add(repeats_title, flag=wx.LEFT | wx.RIGHT, border=10)
+        repeats_sizer.Add(repeats_title, flag=wx.LEFT | wx.RIGHT | wx.ALIGN_CENTER, border=10)
         repeats_sizer.Add(self.repeats_spin, 1, wx.RIGHT, 10)
 
         settings_sizer.Add(td_bot_sizer, flag=wx.EXPAND | wx.BOTTOM, border=11)
@@ -76,7 +76,7 @@ class HomeTab(wx.Panel):
         more_control_sizer.Add(control_sizer, proportion=3)
         main_sizer.Add(more_control_sizer, flag=wx.EXPAND)
 
-        # add a text input field to the bottom
+        # add a message input field to the bottom
         self.msg_input = wx.TextCtrl(self)
         self.msg_input.SetHint('Enter your message here')
         main_sizer.Add(self.msg_input, flag=wx.EXPAND)
